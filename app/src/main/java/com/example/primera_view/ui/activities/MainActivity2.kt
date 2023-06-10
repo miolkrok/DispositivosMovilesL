cee9c604d058bf725a0b0474ee219b70989a3f15
Luis Ortiz <luchoaam97@gmail.com>
2023-06-09 23:40:54 -0500

    cambios de menu

--- app/src/main/java/com/example/primera_view/ui/activities/MainActivity2.kt
+++ app/src/main/java/com/example/primera_view/ui/activities/MainActivity2.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.example.primera_view.R
 import com.example.primera_view.databinding.ActivityMain2Binding
 import com.google.android.material.snackbar.Snackbar
 
@@ -48,6 +49,42 @@ class MainActivity2 : AppCompatActivity() {
             )
 
             startActivity(intent)
+        }
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.inicio -> {
+                    // Respond to navigation item 1 click
+                    var suma: Int =0
+                    for(i in listOf(1,2,3)){
+                        suma = suma+i
+                    }
+                    Snackbar.make(binding.editTextText, "la suma es ${suma}",Snackbar.LENGTH_LONG)
+                        .show()
+
+                    true
+                }
+                R.id.fav -> {
+                    var suma: Int =0
+                    for(i in listOf(8,12,13)){
+                        suma += i
+                    }
+                    // Respond to navigation item 2 click
+                    Snackbar.make(binding.editTextText, "la suma es: ${suma}",Snackbar.LENGTH_LONG)
+                        .show()
+                    true
+                }
+                R.id.api -> {
+                    // Respond to navigation item 2 click
+                    var suma: Int =0
+                    for(i in listOf(30,12,13)){
+                        suma += i
+                    }
+                    Snackbar.make(binding.editTextText, "la suma es: ${suma}",Snackbar.LENGTH_LONG)
+                        .show()
+                    true
+                }
+                else -> false
+            }
         }
     }
 
