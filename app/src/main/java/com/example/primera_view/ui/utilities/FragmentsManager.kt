89b6a9896ae2ec6d36b0bcd67a4ab00eac470cf0
Luis Ortiz <luchoaam97@gmail.com>
2023-06-13 12:26:15 -0500

    cambios 9

---
+++ app/src/main/java/com/example/primera_view/ui/utilities/FragmentsManager.kt
@@ -0,0 +1,31 @@
+package com.example.primera_view.ui.utilities
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+
+class FragmentsManager {
+    fun replaceFragment(
+        manager: FragmentManager,
+        container: Int,
+        fragment: Fragment
+    ){
+        with(manager.beginTransaction()) {
+            replace(container,fragment)
+            addToBackStack(null)
+            commit()
+        }
+    }
+
+
+    fun addFragment(
+        manager: FragmentManager,
+        container: Int,
+        fragment: Fragment
+    ){
+        with(manager.beginTransaction()) {
+            add(container,fragment)
+            addToBackStack(null)
+            commit()
+        }
+    }
+}
